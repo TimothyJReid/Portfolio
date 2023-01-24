fd14fffebb95ee6be5894129a056bb5798936979
Tim Reid <tim.in.canada2014@gmail.com>
2023-01-24 14:53:22 -0500

    Netlify data added to handle form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,6 +64,7 @@ export default function Contact() {
         <form
           data-netlify="true"
           name="contact"
+          method="post"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
