58200ab5349a4cc9901d06d30b5a31e0d61d72cc
Tim Reid <tim.in.canada2014@gmail.com>
2023-01-24 19:03:54 -0500

    Added email mailto click

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,7 +53,10 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a
+                href="mailto:timothyreid00@gmail.com"
+                className="text-indigo-400 leading-relaxed"
+              >
                 timothyreid00@gmail.com
               </a>
             </div>
